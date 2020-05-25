8d8444353292ee80e0da661a1ac4a769f08ca1c2
Brent Yi <yibrenth@gmail.com>
2020-05-25 16:45:33 -0700

    Update LSTM training script

--- scripts/train_door_lstm.py
+++ scripts/train_door_lstm.py
@@ -6,20 +6,27 @@ import crossmodal
 import diffbayes
 import fannypack
 
+# Open PDB before quitting
+fannypack.utils.pdb_safety_net()
+
+# Load training data
+trajectories = crossmodal.door_data.load_trajectories(
+    "panda_door_pull_300.hdf5", "panda_door_push_300.hdf5"
+)
+
 # Create model, Buddy
 filter_model = crossmodal.door_lstm.DoorLSTMFilter()
 buddy = fannypack.utils.Buddy("lstm-test1", filter_model)
 
-
+# Training helper
 def train(*, subsequence_length, batch_size, epochs):
-    trajectories = crossmodal.door_data.load_trajectories("panda_door_pull_10.hdf5")
     dataloader = torch.utils.data.DataLoader(
         diffbayes.data.SubsequenceDataset(
             trajectories=trajectories, subsequence_length=subsequence_length
         ),
         batch_size=batch_size,
         shuffle=True,
-        num_workers=8
+        num_workers=8,
     )
 
     for _ in range(epochs):
@@ -27,7 +34,9 @@ def train(*, subsequence_length, batch_size, epochs):
             buddy, filter_model, dataloader, initial_covariance=torch.eye(3)
         )
 
+# Run training curriculum
+train(subsequence_length=2, batch_size=32, epochs=2)
+buddy.save_checkpoint("phase0")
 
-train(subsequence_length=3, batch_size=32, epochs=5)
-train(subsequence_length=10, batch_size=32, epochs=5)
-train(subsequence_length=20, batch_size=32, epochs=5)
+train(subsequence_length=16, batch_size=32, epochs=20)
+buddy.save_checkpoint("phase1")
