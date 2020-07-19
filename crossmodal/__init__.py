c388790d4059189a2dcd120de9cd62d3b27a5d40
Brent Yi <yibrenth@gmail.com>
2020-07-19 01:22:59 -0700

    Add push train script

--- crossmodal/__init__.py
+++ crossmodal/__init__.py
@@ -1,1 +1,9 @@
-from . import base_models, door_data, door_models, eval_helpers, train_helpers
+from . import (
+    base_models,
+    door_data,
+    door_models,
+    eval_helpers,
+    push_data,
+    push_models,
+    train_helpers,
+)
