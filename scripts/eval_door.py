e46f558e3da02e9337785a81738210cb7f4a5d02
Brent Yi <yibrenth@gmail.com>
2020-07-19 02:21:55 -0700

    Add push scripts

---
+++ scripts/eval_door.py
@@ -0,0 +1,42 @@
+import argparse
+import dataclasses
+
+import crossmodal
+import diffbayes
+import fannypack
+
+# Move cache in case we're running on NFS (eg Juno), open PDB on quit
+fannypack.data.set_cache_path(crossmodal.__path__[0] + "/../.cache")
+fannypack.utils.pdb_safety_net()
+
+# Parse args
+parser = argparse.ArgumentParser()
+parser.add_argument("--experiment-name", type=str)
+parser.add_argument("--checkpoint-label", type=str, default=None)
+parser.add_argument("--save", action="store_true")
+args = parser.parse_args()
+
+# Create Buddy and read experiment metadata
+buddy = fannypack.utils.Buddy(args.experiment_name)
+model_type = buddy.metadata["model_type"]
+dataset_args = buddy.metadata["dataset_args"]
+
+# Load model using experiment metadata
+filter_model: diffbayes.base.Filter = crossmodal.door_models.model_types[model_type]()
+buddy.attach_model(filter_model)
+buddy.load_checkpoint(label=args.checkpoint_label)
+
+# Load trajectories using experiment metadata
+eval_trajectories = crossmodal.door_data.load_trajectories(
+    # "panda_door_pull_10.hdf5", "panda_door_push_10.hdf5", **dataset_args
+    ("panda_door_pull_100.hdf5", 10), ("panda_door_push_100.hdf5", 10), **dataset_args
+)
+
+# Run eval
+eval_helpers = crossmodal.eval_helpers
+eval_helpers.configure(buddy=buddy, trajectories=eval_trajectories)
+eval_results = eval_helpers.run_eval()
+
+# Save eval results
+if args.save:
+    buddy.add_metadata({"eval_results": dataclasses.asdict(eval_results)})
---
+++ scripts/train_push.py
@@ -0,0 +1,167 @@
+import argparse
+import dataclasses
+import datetime
+from typing import cast
+
+import crossmodal
+import fannypack
+from crossmodal.base_models import CrossmodalParticleFilterMeasurementModel
+
+# Index of trainable models
+model_types = crossmodal.push_models.model_types
+
+# Parse args
+parser = argparse.ArgumentParser()
+parser.add_argument("--model-type", type=str, required=True, choices=model_types.keys())
+parser.add_argument("--experiment-name", type=str, required=True)
+parser.add_argument("--notes", type=str, default="(none)")
+crossmodal.push_data.add_dataset_arguments(parser)
+
+# Parse args
+args = parser.parse_args()
+model_type = args.model_type
+dataset_args = crossmodal.push_data.get_dataset_args(args)
+
+# Move cache in case we're running on NFS (eg Juno), open PDB on quit
+fannypack.data.set_cache_path(crossmodal.__path__[0] + "/../.cache")
+fannypack.utils.pdb_safety_net()
+
+# Load trajectories into memory
+train_trajectories = crossmodal.push_data.load_trajectories(
+    "gentle_push_1000.hdf5", **dataset_args
+)
+eval_trajectories = crossmodal.push_data.load_trajectories(
+    "gentle_push_10.hdf5", **dataset_args
+)
+
+# Create model, Buddy
+filter_model = model_types[model_type]()
+buddy = fannypack.utils.Buddy(args.experiment_name, filter_model)
+buddy.set_metadata(
+    {
+        "model_type": model_type,
+        "dataset_args": dataset_args,
+        "train_start_time": datetime.datetime.now().strftime("%b %d, %Y @ %-H:%M:%S"),
+        "commit_hash": fannypack.utils.get_git_commit_hash(crossmodal.__file__),
+        "notes": args.notes,
+    }
+)
+
+# Configure helpers
+train_helpers = crossmodal.train_helpers
+train_helpers.configure(buddy=buddy, trajectories=train_trajectories)
+
+eval_helpers = crossmodal.eval_helpers
+eval_helpers.configure(buddy=buddy, trajectories=eval_trajectories)
+
+# Run model-specific training curriculum
+if isinstance(filter_model, crossmodal.push_models.PushLSTMFilter):
+    # Pre-train for single-step prediction
+    train_helpers.train_e2e(subsequence_length=2, epochs=2, batch_size=32)
+    buddy.save_checkpoint("phase0")
+
+    # Train on longer sequences
+    train_helpers.train_e2e(subsequence_length=4, epochs=5, batch_size=32)
+    eval_helpers.log_eval()
+    train_helpers.train_e2e(subsequence_length=8, epochs=5, batch_size=32)
+    eval_helpers.log_eval()
+    train_helpers.train_e2e(subsequence_length=16, epochs=20, batch_size=32)
+    eval_helpers.log_eval()
+    buddy.save_checkpoint("phase1")
+
+elif isinstance(filter_model, crossmodal.push_models.PushParticleFilter):
+    # Pre-train dynamics (single-step)
+    train_helpers.train_pf_dynamics_single_step(epochs=10)
+    buddy.save_checkpoint("phase0")
+
+    # Pre-train dynamics (recurrent)
+    train_helpers.train_pf_dynamics_recurrent(subsequence_length=4, epochs=5)
+    train_helpers.train_pf_dynamics_recurrent(subsequence_length=8, epochs=5)
+    train_helpers.train_pf_dynamics_recurrent(subsequence_length=16, epochs=5)
+    eval_helpers.log_eval()
+    buddy.save_checkpoint("phase1")
+
+    # Freeze dynamics
+    fannypack.utils.freeze_module(filter_model.dynamics_model)
+
+    # Pre-train measurement model
+    train_helpers.train_pf_measurement(epochs=5, batch_size=64)
+    eval_helpers.log_eval()
+    buddy.save_checkpoint("phase2")
+
+    # Train E2E
+    train_helpers.train_e2e(subsequence_length=4, epochs=5, batch_size=32)
+    eval_helpers.log_eval()
+    train_helpers.train_e2e(subsequence_length=8, epochs=5, batch_size=32)
+    eval_helpers.log_eval()
+    for _ in range(4):
+        train_helpers.train_e2e(subsequence_length=16, epochs=5, batch_size=32)
+        eval_helpers.log_eval()
+    buddy.save_checkpoint("phase3")
+
+elif isinstance(filter_model, crossmodal.push_models.PushCrossmodalParticleFilter):
+    # Pull out measurement model, freeze crossmodal weights
+    measurement_model: CrossmodalParticleFilterMeasurementModel = cast(
+        CrossmodalParticleFilterMeasurementModel, filter_model.measurement_model,
+    )
+    fannypack.utils.freeze_module(measurement_model.crossmodal_weight_model)
+
+    # Pre-train dynamics (single-step)
+    train_helpers.train_pf_dynamics_single_step(epochs=5)
+    buddy.save_checkpoint("phase0")
+
+    # Pre-train dynamics (recurrent)
+    train_helpers.train_pf_dynamics_recurrent(subsequence_length=8, epochs=5)
+    train_helpers.train_pf_dynamics_recurrent(subsequence_length=16, epochs=5)
+    buddy.save_checkpoint("phase1")
+
+    # Freeze dynamics
+    fannypack.utils.freeze_module(filter_model.dynamics_model)
+
+    # Pre-train measurement model (image)
+    measurement_model._enabled_models = [True, False]
+    train_helpers.train_pf_measurement(epochs=3, batch_size=64)
+    train_helpers.train_e2e(subsequence_length=3, epochs=5, batch_size=32)
+    train_helpers.train_e2e(subsequence_length=8, epochs=5, batch_size=32)
+    train_helpers.train_e2e(subsequence_length=16, epochs=20, batch_size=32)
+    buddy.save_checkpoint("phase2")
+
+    # Pre-train measurement model (proprioception + haptics)
+    measurement_model._enabled_models = [False, True]
+    train_helpers.train_pf_measurement(epochs=3, batch_size=64)
+    train_helpers.train_e2e(subsequence_length=3, epochs=5, batch_size=32)
+    eval_helpers.log_eval()
+    train_helpers.train_e2e(subsequence_length=8, epochs=5, batch_size=32)
+    eval_helpers.log_eval()
+    train_helpers.train_e2e(subsequence_length=16, epochs=20, batch_size=32)
+    eval_helpers.log_eval()
+    buddy.save_checkpoint("phase3")
+
+    # Enable both measurement models
+    measurement_model._enabled_models = [True, True]
+
+    # Unfreeze weight model, freeze measurement model
+    fannypack.utils.unfreeze_module(measurement_model.crossmodal_weight_model)
+    fannypack.utils.freeze_module(measurement_model.measurement_models)
+
+    # Train everything end-to-end
+    train_helpers.train_e2e(subsequence_length=3, epochs=5, batch_size=32)
+    eval_helpers.log_eval()
+    train_helpers.train_e2e(subsequence_length=8, epochs=5, batch_size=32)
+    eval_helpers.log_eval()
+    for _ in range(4):
+        train_helpers.train_e2e(subsequence_length=16, epochs=5, batch_size=32)
+        eval_helpers.log_eval()
+    buddy.save_checkpoint("phase4")
+
+else:
+    assert False, "No training curriculum found for model type"
+
+# Add training end time
+buddy.add_metadata(
+    {"train_end_time": datetime.datetime.now().strftime("%b %d, %Y @ %-H:%M:%S"),}
+)
+
+# Eval model when done
+eval_results = crossmodal.eval_helpers.run_eval()
+buddy.add_metadata({"eval_results": dataclasses.asdict(eval_results)})
