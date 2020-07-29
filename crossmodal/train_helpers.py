aae0195da14f5c70c0ec3b8f86357020d636fdc3
Michelle Lee <amichlee@gmail.com>
2020-07-29 13:58:11 -0700

    train cm filter logging weights

--- crossmodal/train_helpers.py
+++ crossmodal/train_helpers.py
@@ -147,3 +147,168 @@ def train_e2e(*, subsequence_length, epochs, batch_size=32, initial_cov_scale=0.
             measurement_initialize=measurement_initialize,
             optimizer_name=optimizer_name
         )
+
+
+
+"""
+BELOW IS PROTOTYPING CODE FOR TRAINING CROSSMODAL EKFs
+
+"""
+
+import torch.nn.functional as F
+from tqdm.auto import tqdm
+
+def _swap_batch_sequence_axes(tensor):
+    """Converts data formatted as (N, T, ...) to (T, N, ...)
+    """
+    return torch.transpose(tensor, 0, 1)
+
+
+def train_crossmodal_filter(
+    buddy: fannypack.utils.Buddy,
+    filter_model: diffbayes.base.Filter,
+    dataloader,
+    initial_covariance: torch.Tensor,
+    *,
+    loss_function= F.mse_loss,
+    log_interval: int = 10,
+    measurement_initialize=False,
+    optimizer_name="train_filter_recurrent",
+) -> None:
+    """Trains a filter end-to-end via backpropagation through time for 1 epoch over a
+    subsequence dataset.
+
+    """
+    # Dataloader should load a SubsequenceDataset
+    assert isinstance(dataloader.dataset, diffbayes.data.SubsequenceDataset)
+    assert initial_covariance.shape == (filter_model.state_dim, filter_model.state_dim)
+    assert filter_model.training, "Model needs to be set to train mode"
+
+    # Track mean epoch loss
+    epoch_loss = 0.0
+
+    # Train filter model for 1 epoch
+    with buddy.log_scope("train_filter_recurrent"):
+        for batch_idx, batch_data in enumerate(tqdm(dataloader)):
+            # Move data
+            batch_gpu = fannypack.utils.to_device(batch_data, buddy.device)
+            true_states, observations, controls = batch_gpu
+
+            # Swap batch size, sequence length axes
+            true_states = _swap_batch_sequence_axes(true_states)
+            observations = fannypack.utils.SliceWrapper(observations).map(
+                _swap_batch_sequence_axes
+            )
+            controls = fannypack.utils.SliceWrapper(controls).map(
+                _swap_batch_sequence_axes
+            )
+
+            # Shape checks
+            T, N, state_dim = true_states.shape
+            assert state_dim == filter_model.state_dim
+            assert fannypack.utils.SliceWrapper(observations).shape[:2] == (T, N)
+            assert fannypack.utils.SliceWrapper(controls).shape[:2] == (T, N)
+            assert batch_idx != 0 or N == dataloader.batch_size
+
+            # Populate initial filter belief
+            if measurement_initialize and hasattr(filter_model, 'measurement_initialize_beliefs'):
+                filter_model.measurement_initialize_beliefs(fannypack.utils.SliceWrapper(observations)[0])
+            else:
+                initial_states_covariance = initial_covariance[None, :, :].expand(
+                    (N, state_dim, state_dim)
+                )
+                scale_tril = torch.sqrt(initial_states_covariance)
+                initial_states = torch.distributions.MultivariateNormal(
+                    true_states[0], scale_tril=scale_tril,
+                ).sample()
+                filter_model.initialize_beliefs(
+                    mean=initial_states, covariance=initial_states_covariance
+                )
+
+            def train_loop(obs, c, cm_filter):
+                obs = fannypack.utils.SliceWrapper(obs)
+                c = fannypack.utils.SliceWrapper(c)
+
+                # Get sequence length (T), batch size (N)
+                T, N = c.shape[:2]
+                assert obs.shape[:2] == (T, N)
+
+                # Filtering forward pass
+                # We treat t = 0 as a special case to make it easier to create state_predictions
+                # tensor on the correct device
+                t = 0
+                current_prediction = cm_filter(observations=obs[t], controls=c[t])
+                state_pred = current_prediction.new_zeros((T, N, cm_filter.state_dim))
+                state_pred[t] = current_prediction
+                weight_pred = current_prediction.new_zeros((T, N, cm_filter.state_dim))
+
+                for t in range(1, T):
+                    # Compute state prediction for a single timestep
+                    # We use __call__ to make sure hooks are dispatched correctly
+                    current_prediction = cm_filter(
+                        observations=obs[t], controls=c[t]
+                    )
+
+                    # Validate & add to output
+                    state_pred[t] = current_prediction
+                    weight_pred[t] = cm_filter.crossmodal_weight_model(
+                        observations=obs[t]
+                    )
+
+                return state_pred, weight_pred
+
+            state_predictions, weight_predictions = train_loop(
+                fannypack.utils.SliceWrapper(observations)[1:],
+                fannypack.utils.SliceWrapper(controls)[1:],
+                filter_model,
+            )
+
+            # Minimize loss
+            loss = loss_function(state_predictions, true_states[1:])
+            buddy.minimize(loss, optimizer_name=optimizer_name)
+            epoch_loss += fannypack.utils.to_numpy(loss)
+
+            # Logging
+            if batch_idx % log_interval == 0:
+                buddy.log("loss", loss)
+                buddy.log_gradient_norm()
+                buddy.log("Pred  mean", state_predictions.mean())
+                buddy.log("Pred  std", state_predictions.std())
+                buddy.log("Weight Pred mean", weight_predictions.mean())
+                buddy.log("Weight Pred std", weight_predictions.std())
+
+    # Print average training loss
+    epoch_loss /= len(dataloader)
+    print("(train_filter) Epoch training loss: ", epoch_loss)
+
+
+def train_cm_e2e(*, subsequence_length, epochs, batch_size=32, initial_cov_scale=0.1,
+              measurement_initialize=False, model=None,
+              optimizer_name="train_filter_recurrent"):
+
+    if model is None:
+        model = filter_model
+    assert isinstance(model, diffbayes.base.Filter)
+
+    # Put model in train mode
+    model.train()
+
+    dataloader = torch.utils.data.DataLoader(
+        diffbayes.data.SubsequenceDataset(
+            trajectories=trajectories, subsequence_length=subsequence_length
+        ),
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=True,
+    )
+    initial_covariance = (
+        torch.eye(model.state_dim, device=buddy.device) * initial_cov_scale
+    )
+    for _ in range(epochs):
+        train_crossmodal_filter(
+            buddy, model, dataloader,
+            initial_covariance=initial_covariance,
+            measurement_initialize=measurement_initialize,
+            optimizer_name=optimizer_name
+        )
---
+++ scripts/push_task/train_weights.py
@@ -0,0 +1,131 @@
+import argparse
+import dataclasses
+import datetime
+from typing import cast
+
+import crossmodal
+import diffbayes
+import fannypack
+from crossmodal.base_models import CrossmodalKalmanFilter
+
+Task = crossmodal.tasks.PushTask
+
+# Move cache in case we're running on NFS (eg Juno), open PDB on quit
+fannypack.data.set_cache_path(crossmodal.__path__[0] + "/../.cache")
+fannypack.utils.pdb_safety_net()
+
+# Parse args
+parser = argparse.ArgumentParser()
+parser.add_argument("--experiment-name", type=str)
+parser.add_argument("--checkpoint-label", type=str, default=None)
+parser.add_argument("--save", action="store_true")
+parser.add_argument("--original-experiment", type=str, default=None)
+parser.add_argument("--unimodal", action="store_true")
+
+Task.add_dataset_arguments(parser)
+# Parse args
+args = parser.parse_args()
+# Create Buddy and read experiment metadata
+buddy = fannypack.utils.Buddy(args.experiment_name)
+
+
+if args.original_experiment is not None:
+    buddy_original = fannypack.utils.Buddy(args.original_experiment)
+
+    try:
+        model_type = buddy_original.metadata["model_type"]
+        dataset_args = buddy_original.metadata["dataset_args"]
+
+        buddy.set_metadata(buddy_original.metadata)
+
+    except:
+        print("missing metadata")
+
+else:
+    try:
+        model_type = buddy.metadata["model_type"]
+        dataset_args = buddy.metadata["dataset_args"]
+    except:
+        print("missing metadata")
+
+
+
+filter_model: diffbayes.base.Filter = crossmodal.push_models.model_types[model_type]()
+buddy.attach_model(filter_model)
+buddy.load_checkpoint(label=args.checkpoint_label, experiment_name=args.original_experiment)
+
+
+# Load trajectories into memory
+train_trajectories = Task.get_train_trajectories(**dataset_args)
+eval_trajectories = Task.get_eval_trajectories(**dataset_args)
+
+# Configure helpers
+train_helpers = crossmodal.train_helpers
+train_helpers.configure(buddy=buddy, trajectories=train_trajectories)
+
+eval_helpers = crossmodal.eval_helpers
+eval_helpers.configure(buddy=buddy, trajectories=eval_trajectories, task=Task)
+
+# Run model-specific training curriculum
+if isinstance(filter_model, crossmodal.push_models.PushCrossmodalKalmanFilter):
+    image_model = filter_model.filter_models[0]
+    force_model = filter_model.filter_models[1]
+
+    filter_model.enabled_models = [True, True]
+
+    # Unfreeze weight model, freeze filter model
+    fannypack.utils.unfreeze_module(filter_model.crossmodal_weight_model)
+    fannypack.utils.freeze_module(filter_model.filter_models)
+
+    train_helpers.train_cm_e2e(subsequence_length=3, epochs=5, batch_size=32,
+                            optimizer_name="freeze_ekf")
+    eval_helpers.log_eval()
+
+    train_helpers.train_cm_e2e(subsequence_length=4, epochs=5, batch_size=32,
+                            optimizer_name="freeze_ekf")
+    eval_helpers.log_eval()
+
+    buddy.save_checkpoint("finetune_phase4-freeze")
+
+    # Train everything end-to-end
+    fannypack.utils.unfreeze_module(filter_model.filter_models)
+
+    train_helpers.train_cm_e2e(subsequence_length=3, epochs=5,
+                            batch_size=32, measurement_initialize=False)
+    eval_helpers.log_eval()
+    buddy.save_checkpoint("finetune_phase4-length3")
+
+    buddy.set_regularization_weight(
+        optimizer_name="train_filter_recurrent", value=0.0001
+    )
+
+    for _ in range(3):
+
+        train_helpers.train_cm_e2e(
+            subsequence_length=4, epochs=5, batch_size=32, measurement_initialize=True
+        )
+        eval_helpers.log_eval()
+    buddy.save_checkpoint("finetune_phase4-length4")
+
+    for _ in range(2):
+
+        train_helpers.train_cm_e2e(
+            subsequence_length=6, epochs=5, batch_size=32, measurement_initialize=True
+        )
+        eval_helpers.log_eval()
+        print("kalman e2e")
+
+    buddy.save_checkpoint("finetune_phase4-done")
+
+
+else:
+    assert False, "No training curriculum found for model type"
+
+# Add training end time
+# buddy.add_metadata(
+#     {"train_end_time": datetime.datetime.now().strftime("%b %d, %Y @ %-H:%M:%S"),}
+# )
+
+# Eval model when done
+eval_results = crossmodal.eval_helpers.run_eval()
+buddy.add_metadata({"eval_results": eval_results})
